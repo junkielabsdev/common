fabf757be7fcb0dc114ddbb82403fb2e446acaa7
nirajprakash <nirajprakash13@gmail.com>
2022-03-30 18:12:06 +0530

    added privacy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './default/default.component';
+import { PrivacyComponent } from './privacy/privacy/privacy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "privacy",
+    component: PrivacyComponent
+  },
+  {
+    path: "",
+    component: DefaultComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
